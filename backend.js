47637c2c4c29745cf8285cea680620ac1a3b6041
itsmeow <mods.itsmeow@gmail.com>
2020-12-16 19:59:52 -0500

    Update to be pog

--- backend.js
+++ backend.js
@@ -1,58 +1,41 @@
-require("dotenv").config()
-const AUTH_KEY = process.env.AUTHORIZATION_KEY
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const express = require("express")
+const mariadb = require("mariadb")
 const app = express()
-
-let temperature = 0
-let humidity = 0
-
-const authenticate = auth => {
-  return (
-    auth !== undefined &&
-    auth !== null &&
-    auth !== "" &&
-    AUTH_KEY !== "" &&
-    AUTH_KEY !== undefined &&
-    AUTH_KEY !== null &&
-    AUTH_KEY === auth
-  )
-}
+const pool = mariadb.createPool({
+  host: "127.0.0.1",
+  user: "tempsense",
+  connectionLimit: 5,
+})
 
 app.use(cors())
 app.use(bodyParser.json())
-app.post("/api/update-data", (req, res) => {
-  const auth = req.header("Authorization")
-  if (authenticate(auth)) {
-    // authenticated!
-    if (
-      typeof req.body.temperature === "number" &&
-      typeof req.body.humidity === "number"
-    ) {
-      temperature = req.body.temperature
-      humidity = req.body.humidity
-      res.json({
-        temperature: temperature,
-        humidity: humidity,
-      })
-    } else {
-      res.sendStatus(400)
-    }
-  } else {
-    res.sendStatus(403)
+app.get("/api/get-data", async (req, res) => {
+  let conn
+  try {
+    conn = await pool.getConnection()
+    const rows = await conn.query(
+      "SELECT UNIX_TIMESTAMP(at) AS at, (temp * (9.0 / 5) + 32) AS temp, humidity FROM `temperature`.`temperature` WHERE at >= NOW() - INTERVAL 12 HOUR"
+    )
+    let lastEntry = rows[rows.length - 1]
+    res.json({
+      lastTemperature: lastEntry.temp,
+      lastHumidity: lastEntry.humidity,
+      history: rows,
+    })
+  } catch (err) {
+    res.writeHead(500)
+    res.end()
+    throw err
+  } finally {
+    if (conn) conn.end()
   }
-})
-app.get("/api/get-data", (req, res) => {
-  res.json({
-    temperature: temperature,
-    humidity: humidity,
-  })
 })
 
 if (process.env.NODE_ENV !== "development") {
   app.use(express.static("public"))
 }
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port)
-console.log(`Listening on port ${port}`);
+console.log(`Listening on port ${port}`)
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,40 +2,51 @@ import React, { useEffect, useState } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import ClientOnly from "../components/clientonly"
-import { LineChart, Line, XAxis, YAxis } from "recharts"
 import moment from "moment"
 import useWindowSize from "../hooks/useWindowSize"
-
+import {
+  VictoryChart,
+  VictoryLine,
+  VictoryAxis,
+  VictoryTheme,
+  VictoryBrushContainer,
+  VictoryTooltip,
+  createContainer,
+} from "victory"
+const DEV_MODE = process.env.NODE_ENV
 const IndexPage = () => {
   const [temp, setTemp] = useState(0)
   const [humidity, setHumidity] = useState(0)
   const [tempData, setTempData] = useState([])
+  const [zoomDomain, setZoomDomain] = useState({ x: [0, 1] })
+  const [zoomDomain2, setZoomDomain2] = useState({ x: [0, 1] })
   const size = useWindowSize()
   useEffect(() => {
     const updateData = () => {
       if (window !== undefined) {
-        fetch("http://localhost:3000/api/get-data").then(data => {
+        fetch(
+          (DEV_MODE === "development" ? "http://localhost:3000" : "") +
+            "/api/get-data"
+        ).then(data => {
           data.json().then(json => {
-            setTemp(json.temperature)
-            setHumidity(json.humidity)
-            setTempData(d => {
-              let copy = [...d]
-              if (
-                copy.length <= 0 ||
-                copy[copy.length - 1].y !== json.temperature
-              ) {
-                copy.push({ time: new Date().getTime(), y: json.temperature })
-              }
-              return copy
-            })
+            setTemp(json.lastTemperature)
+            setHumidity(json.lastHumidity)
+            setTempData(json.history)
+            let last = json.history[json.history.length - 1]
+            let domain = {
+              x: [moment.unix(last.at).subtract(2, "h").unix(), last.at],
+            }
+            setZoomDomain(domain)
+            setZoomDomain2(domain)
           })
         })
       }
     }
-    const interval = setInterval(updateData, 60000)
+    const interval = setInterval(updateData, 30000)
     updateData()
     return () => clearInterval(interval)
   }, [])
+  const VictoryZoomVoronoiContainer = createContainer("zoom", "voronoi")
   return (
     <Layout>
       <SEO title="Home" />
@@ -47,38 +58,186 @@ const IndexPage = () => {
           <strong>Humidity:</strong> {humidity}­­%
         </div>
         <hr />
-        <h2>Temperature (°F)</h2>
-        <LineChart
-          width={size.width * 0.8}
-          height={size.height * 0.7}
-          data={tempData}
-        >
-          <XAxis
-            dataKey="time"
-            domain={["auto", "auto"]}
-            name="Time"
-            tickFormatter={unixTime =>
-              moment(unixTime).format("h:mma [(]s[s)]")
-            }
-            type="number"
-            tick={{ fill: "#e8e8e8", fontSize: "0.95rem" }}
-          />
-          <YAxis
-            tick={{ fill: "#e8e8e8" }}
-            type="number"
-            name="Temperature (°F)"
-            tickFormatter={t => `${t}°F`}
-          />
-          <Line
-            dot={{ fill: "#0c0c0c", stroke: "#e8e8e8" }}
-            activeDot={false}
-            animationDuration={0}
-            type="monotone"
-            dataKey="y"
-            stroke="#FF0000"
-            stokeWidth={10}
-          />
-        </LineChart>
+        <div className="chart-wrap">
+          <h2>Temperature</h2>
+          <div className="chart-top">
+            <svg style={{ height: 0 }}>
+              <defs>
+                <linearGradient
+                  id="tempGradient"
+                  x1="0%"
+                  y1="0%"
+                  x2="0%"
+                  y2="100%"
+                >
+                  <stop offset="0%" stopColor="red" />
+                  <stop offset="25%" stopColor="orange" />
+                  <stop offset="50%" stopColor="yellow" />
+                  <stop offset="75%" stopColor="green" />
+                  <stop offset="100%" stopColor="blue" />
+                </linearGradient>
+              </defs>
+            </svg>
+            <VictoryChart
+              width={size.width / 2}
+              height={size.height - 24 * 3 - 12.8 * 3 - 12 - 29 - 19.92 * 2}
+              theme={VictoryTheme.material}
+              scale={{ x: "time" }}
+              containerComponent={
+                <VictoryZoomVoronoiContainer
+                  allowZoom={false}
+                  zoomDomain={zoomDomain}
+                  onZoomDomainChange={setZoomDomain}
+                  labels={d =>
+                    `${moment.unix(d.datum._x).format("ddd hh:mma")} - ${
+                      d.datum._y
+                    }°F`
+                  }
+                />
+              }
+            >
+              <VictoryAxis
+                tickFormat={x => moment.unix(x).format("ddd hh:mma")}
+              />
+              <VictoryAxis
+                dependentAxis
+                standalone={false}
+                tickValues={[
+                  32,
+                  40,
+                  45,
+                  50,
+                  55,
+                  60,
+                  65,
+                  70,
+                  75,
+                  80,
+                  85,
+                  90,
+                  95,
+                  100,
+                ]}
+                tickFormat={y => y + "°F"}
+              />
+              <VictoryLine
+                labelComponent={<VictoryTooltip />}
+                style={{
+                  data: { stroke: "url(#tempGradient)" },
+                  parent: { border: "1px solid #ccc" },
+                }}
+                data={tempData}
+                x="at"
+                y="temp"
+                domain={{ y: [32, 100] }}
+              />
+            </VictoryChart>
+          </div>
+          <div className="chart-bottom">
+            <VictoryChart
+              padding={{ top: 0, left: 50, right: 50, bottom: 50 }}
+              width={size.width * (3 / 4)}
+              theme={VictoryTheme.material}
+              height={100}
+              scale={{ x: "time" }}
+              containerComponent={
+                <VictoryBrushContainer
+                  brushDimension="x"
+                  brushDomain={zoomDomain}
+                  onBrushDomainChange={setZoomDomain}
+                />
+              }
+            >
+              <VictoryAxis
+                tickFormat={x => moment.unix(x).format("ddd hh:mma")}
+              />
+              <VictoryLine
+                style={{
+                  data: { stroke: "#c43a31" },
+                  parent: { border: "1px solid #ccc" },
+                }}
+                data={tempData}
+                x="at"
+                y="temp"
+                domain={{ y: [32, 100] }}
+              />
+            </VictoryChart>
+          </div>
+        </div>
+        <div className="chart-wrap">
+          <h2>Humidity</h2>
+          <div className="chart-top">
+            <VictoryChart
+              width={size.width / 2}
+              height={size.height - 24 * 3 - 12.8 * 3 - 12 - 29 - 19.92 * 2}
+              theme={VictoryTheme.material}
+              scale={{ x: "time" }}
+              containerComponent={
+                <VictoryZoomVoronoiContainer
+                  allowZoom={false}
+                  zoomDomain={zoomDomain2}
+                  onZoomDomainChange={setZoomDomain2}
+                  labels={d =>
+                    `${moment.unix(d.datum._x).format("ddd hh:mma")} - ${
+                      d.datum._y
+                    }%`
+                  }
+                />
+              }
+            >
+              <VictoryAxis
+                tickFormat={x => moment.unix(x).format("ddd hh:mma")}
+              />
+              <VictoryAxis
+                dependentAxis
+                standalone={false}
+                tickValues={[20, 30, 40, 50, 60, 70, 80]}
+                tickFormat={y => y + "%"}
+              />
+              <VictoryLine
+                labelComponent={<VictoryTooltip />}
+                style={{
+                  data: { stroke: "#c43a31" },
+                  parent: { border: "1px solid #ccc" },
+                }}
+                data={tempData}
+                x="at"
+                y="humidity"
+                domain={{ y: [20, 80] }}
+              />
+            </VictoryChart>
+          </div>
+          <div className="chart-bottom">
+            <VictoryChart
+              padding={{ top: 0, left: 50, right: 50, bottom: 50 }}
+              width={size.width * (3 / 4)}
+              theme={VictoryTheme.material}
+              height={100}
+              scale={{ x: "time" }}
+              containerComponent={
+                <VictoryBrushContainer
+                  brushDimension="x"
+                  brushDomain={zoomDomain2}
+                  onBrushDomainChange={setZoomDomain2}
+                />
+              }
+            >
+              <VictoryAxis
+                tickFormat={x => moment.unix(x).format("ddd hh:mma")}
+              />
+              <VictoryLine
+                style={{
+                  data: { stroke: "#c43a31" },
+                  parent: { border: "1px solid #ccc" },
+                }}
+                data={tempData}
+                x="at"
+                y="humidity"
+                domain={{ y: [20, 80] }}
+              />
+            </VictoryChart>
+          </div>
+        </div>
       </ClientOnly>
     </Layout>
   )
